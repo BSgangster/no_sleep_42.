26ab30eb28271188aa17921eb8ef8b37049392e8
sphe <sphe@localhost.localdomain>
2019-05-08 13:00:24 +0200

    To lower and To upper -Sphe

---
+++ libft/src/ft_tolower.c
@@ -0,0 +1,8 @@
+//easy stuff
+
+char ft_tolower(char c)
+{
+    if(c >= 'A' && c <= 'Z')
+        return(c + 32);
+    return(c);
+}
---
+++ libft/src/ft_touppper.c
@@ -0,0 +1,7 @@
+
+char    ft_toupper(char c)
+{
+    if(c >= 'a' && c <= 'z') //if the given string or character has characters that are within the range of these lower case characters 
+        return(c - 32); //subtract 32 which will bring it to the uppercase letter in the ascii
+    return(c);       
+}
