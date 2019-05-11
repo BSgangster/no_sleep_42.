c404722cf64d52ec304a7da974bfbc36782ffabf
Iveno Carolus <46677252+BSgangster@users.noreply.github.com>
2019-05-11 11:32:26 +0200

    edited by - Iveno

--- libft/src/ft_isdigit.c
+++ libft/src/ft_isdigit.c
@@ -2,7 +2,7 @@
 
 int ft_isdigit(int c)
 {
-    if(c >= 0 && c <= 9 || c >= '0' && c <= '9') 
+    if(c >= '0' && c <= '9') //Just use the one statment saying c >= 0 && c <= 9 is redundant to c >= '0' && c <= '9', as '0' and '9' are actually just the integers 0 and 9.
     {
         return(1);
     }
