7294e68f33f4322682202f53ecef1280a12f7696
Iveno Carolus <46677252+BSgangster@users.noreply.github.com>
2019-05-11 15:08:00 +0200

    ft_memccopy.c by - Iveno

---
+++ libft/src/ft_memccopy.c
@@ -0,0 +1,36 @@
+#include "libft.h"
+
+//basically this is just memcopy, however , we stop copying when reach either value c, or we get to the n size of bytes.
+void *ft_memccopy(void *dest, const void *src, int c, size_t n)
+{
+    unsigned char *s;
+    unsigned char *usrc;
+    int i;
+    
+    i = 0;
+    s = (unsigned char *)dest;
+    usrc = (unsigned char *)src;
+    while(n--)
+    {
+        if(usrc[i] == (unsigned char)c)
+        {
+            s[i + 1] = c;
+            return &s;
+        }
+        s[i] = usrc[i];
+        i++;
+    }
+    return NULL;
+}
+
+/* Just some test code below , use follwing libs , stdio.h and string.h
+int main(void)
+{
+    char b[11] = "beczuseior";
+    char a[20];
+    
+    ft_memccopy(a,b,'a',7 * sizeof(char));
+    //memccopy(a,b,'a',7 * sizeof(char));
+    printf("%s",a);
+}
+*/
