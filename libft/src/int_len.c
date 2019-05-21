d6a03f3a35178ad21f3562e6f8e521fbce439698
Iveno Carolus <46677252+BSgangster@users.noreply.github.com>
2019-05-21 22:36:03 +0200

    int_len.c - Iveno

---
+++ libft/src/int_len.c
@@ -0,0 +1,23 @@
+//gets the length of an actual integer not a string
+//not going to explain this one it's fairly simple :)
+int int_len(int n)
+{
+    int i;
+    
+    i = 0;
+    if(n < 0)
+    {
+        i++;
+        n *= -1;
+    }
+    if(n == 0)
+    {
+        return 1;
+    }
+    while(n > 0)
+    {
+    i++;
+    n /= 10;
+    }
+    return i;
+}
