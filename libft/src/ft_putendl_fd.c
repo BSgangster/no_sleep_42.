ace28876aea3f2dfcd8e42e5052c18ee1f4beb4e
Iveno Carolus <46677252+BSgangster@users.noreply.github.com>
2019-05-21 18:47:43 +0200

    ft_putendl_fd.c - Iveno

---
+++ libft/src/ft_putendl_fd.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+//writing every character in the string, using the given file description.
+void ft_putendl_fd(const char *str, int fd)
+{
+    while(*str)
+    {
+        write(fd,*str,1);
+        str++;
+    }
+    write(fd,"\n",1);
+}
