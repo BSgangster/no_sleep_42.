76684c9c80c9fe3b16cca6bc93c44bfa5187ea2d
Shane Van Niekerk <svan-nie@c3r6s4.wethinkcode.co.za>
2019-05-21 10:19:42 +0200

    changed the ft_memset to the understanding i have of it

--- libft/src/ft_memset.c
+++ libft/src/ft_memset.c
@@ -13,16 +13,18 @@
  * this will fill word up A's until the 8th index. Remember the char A is actaully an integer value of 65 or something, I think, fuck who knows
  */
 
-void *ft_memset(void *src, int x, size_t n)
+void *ft_memset(void *str, int x, size_t n)
 {
-	unsigned char *start;
+	unsigned char *ptr;
+	size_t count;
 
-       	start = (unsigned char*)src;//gives us that 0-255 char range,letting us treat chars as ints as well.
-	while(n--)
+	ptr = str; // pointing to the string we useing that is pointer to the block of memory to fill.
+	count = 0; // count we going to use to move through the string
+	while(count < n)// while the count is smaller than n it will replace the space in memory with x
 	{
-		*src = x;
-		src++;
+		ptr[count] = x; // changing each space in memory with x
+		count++;
 	}
-	return start;
+	return (str); //returning the str with the replaced memory.
 }
 //well, i assume since memset only fills up space in memory we do not have to add any null vals to the end of the fill ups. Correct me if i'm wrong tho.
