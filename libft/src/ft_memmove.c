771d64c074931e93b502b93a05b1e9d97fd8dc14
Iveno CAROLUS <icarolus@c3r5s1.wethinkcode.co.za>
2019-05-27 17:13:56 +0200

    updating ft_memmove.c

--- libft/src/ft_memmove.c
+++ libft/src/ft_memmove.c
@@ -1,30 +1,29 @@
 #include <stdlib.h>
 #include "libft.h"
-
-//memmove is like memcpy but instead of copying everything one by one, we copy them into a buffer , and then into our destination array.
-//Why? Because this way we can deal with cases where addresses overlap.
+//memmove is basically memcpy but it handles the cases of an overlap happening.
 void *ft_memmove(void *dest,const void *src, size_t n)
 {
-    unsigned char *bufferDest = NULL;//in this case we need a buffer because we'll deal with overlapping data
-    
-    unsigned char *udest;//once again we don't wanna work with void types so we cast to unsigned chars
-    unsigned char *usrc;
-    
-    udest = (unsigned char *)dest;
-    usrc = (unsigned char *)src;
-    
-    bufferDest = malloc(n * sizeof(unsigned char));//we allocate the space that should be used bufferDest
-    
-    if(bufferDest == NULL)//if bufferDest is still just a pointer pointing to NULL end this program now
-    {
-        return NULL;    
-    }
-    else{
-    ft_memcpy(bufferDest, usrc,n);//copy the n amount of things from usrc, in order, into bufferDest
-    ft_memcpy(udest,bufferDest,n);//then from there copy the things from bufferDest into ft_memcopy
-    //remember that the reason bufferDest exists is because if we just used the two arrays udest and usrc we'd have a problem with overlapping addresses.
-    free(bufferDest);
-    }
-    
-    return dest;
+	unsigned int	i;
+
+	i = 0;
+	//check if src does not overlap dest
+	if((unsigned char *)src < (unsigned char *)dest)
+	{
+		//if it doesnt then just copy the corresponding values of src to dest
+		while(n-- > 0)
+		{
+			(unsigned char*)dest[len] = (unsigned char*)src[len];
+		}
+	}
+	//if it does overlap however , do eitherwise
+	else
+	{
+		//copy whatever us in src into dest, but stop as soon as you reach n amount.
+		while(i < n)
+		{
+			(unsigned char *)dest[len] = (unsigned char*)src[len];
+			i++;
+		}
+	}
+	return ((unsigned char *)dest);
 }
