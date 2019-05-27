67a33da4254919b45d1b2ff1301ffef0f992db25
Iveno CAROLUS <icarolus@c3r5s1.wethinkcode.co.za>
2019-05-27 17:47:48 +0200

    adding bonus part feature

--- headerfile_maker/header_maker.py
+++ headerfile_maker/header_maker.py
@@ -29,7 +29,16 @@ header_file  = open('libft.h', 'w')
 header_file.write('#ifndef LIBFT_H' + '\n')
 header_file.write('# define LIBFT_H' + '\n')
 header_file.write('\n')
+
+#including the header files
 header_file.write('\\\\Place your imports/includes over here' + '\n')
+header_file.write('#include <unistd.h>\n')
+header_file.write('#include <stdlib.h>\n')
+
+#defining the structs
+x =str(raw_input("Did you do the bonus question?[Y/N] : "))
+if x == 'Y':
+    header_file.write('typedef struct       s_list\n{\n     void        *content;\n     size_t      content_size;\n     struct s_list       *next;\n}                   t_list;\n')
 
 for i in proto_list:
     header_file.write(proto_list[counter] + ';' + '\n')
