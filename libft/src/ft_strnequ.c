5092b8414dc5aebab8893663e63506517d075e70
BSGangster <bubblesortguru@gmail.com>
2019-05-28 15:28:59 +0200

    ft_strnequ.c - Iveno

---
+++ libft/src/ft_strnequ.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+int     ft_strnequ(const char *str,const char *str1,size_t n)
+{
+        int i;
+
+        i = 0;
+        if (!(str && str1))
+                return(0);
+	if(!n)
+		return(0);
+        if(ft_strlen(str) != strlen(str1))
+                return(0);
+        while(str[i] && (i < n))
+        {
+                if(str[i] == str1[i])
+                        i++;
+                else
+                        return(0);
+        }
+        return(1);
+}
