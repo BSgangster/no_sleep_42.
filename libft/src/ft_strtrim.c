00843c69e9debfefef470f96e7f5762799e1eb62
BSGangster <bubblesortguru@gmail.com>
2019-05-28 13:41:05 +0200

    adding iswhitespace and strtrim functions

---
+++ libft/src/ft_strtrim.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//very simple function, just trims out all the whitespaces in the beginning or end of a string.
+int ft_iswhitespace(char c);
+char	*ft_strtrim(const char *s)
+{
+	char *newstring;
+	size_t newstring_len;
+	size_t i;
+	size_t x;
+	
+	x = 0;
+	newstring_len = strlen(s);
+	if(!s)
+	{
+		return (NULL);
+	}
+	//let's get the white spaces infront of the string
+	while(ft_iswhitespace(s[i]))
+		i++;
+	//let's erase the spaces at the end of the string.
+	while(ft_iswhitespace(s[newstring_len--]))	
+		continue;	
+	newstring_len - i;
+	newstring = malloc(newstring_len * sizeof(char) + 1);
+	//let's get the on empty space stuff
+	while(s[i] && (i <= newstring_len))
+	{
+		newstring[x] = s[i];
+		x++;
+		i++;
+	}
+	newstring[x] = '\0';
+	return (newstring);
+}
