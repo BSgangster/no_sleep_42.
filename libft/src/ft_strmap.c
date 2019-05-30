0c6baab498706c0221e30060dc404abcedcd9d17
BSGangster <bubblesortguru@gmail.com>
2019-05-30 13:41:38 +0200

    ft_strmap.c

---
+++ libft/src/ft_strmap.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+//maps out an array that has function f, applied to every single character in the source array.
+char	*ft_strmap(const char *str,char (*f)(char))
+{
+	int	i;
+	size_t	len;
+	char	*new_str;
+
+	if(!str)
+		return (NULL);
+	i = 0;
+	len = ft_strlen(str);
+	new_str = malloc(len * sizeof(char) + 1);
+	if(!new_str)
+		return (NULL);
+	while(i < len)
+	{
+		new_str[i] = f(str[i]);
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
